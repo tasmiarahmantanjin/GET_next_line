3af6b323c4c09993fb513bfff9248ad893264231
Tasmia Rahman <tasmiata@gmail.com>
2020-08-16 14:36:04 +0300

    add header file

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trahman <trahman@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 17:14:27 by trahman           #+#    #+#             */
+/*   Updated: 2020/07/20 17:02:27 by trahman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 10
+# define FD_SIZE 4864
+
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
